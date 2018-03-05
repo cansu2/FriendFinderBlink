5bc40a7eaca4cec36da2a25f815842b47924fc53
cansu2 <alacansu@gmail.com>
2018-03-04 22:30:33 -0700

    modal doesnot work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,36 @@
 var friends =[
     {
         name:"Cansu",
-        photo: "link",
+        photo: "https://media.giphy.com/media/l3mZcqogHCmOCvsVq/giphy.gif",
         scores: ["1","3","3","5","4","5","2","3","4","5"]
+    },
+    {
+        name:"Jonny",
+        photo: "https://media.giphy.com/media/l3mZcqogHCmOCvsVq/giphy.gif",
+        scores: ["1","2","3","3","4","5","3","4","4","5"]
+    },
+    {
+        name:"Flower",
+        photo: "https://media.giphy.com/media/l3mZcqogHCmOCvsVq/giphy.gif",
+        scores: ["5","3","4","5","3","5","2","1","2","5"]
+    },
+    {
+        name:"Pete",
+        photo: "https://media.giphy.com/media/l3mZcqogHCmOCvsVq/giphy.gif",
+        scores: ["1","4","3","2","4","5","2","3","1","1"]
+    },
+    {
+        name:"Can",
+        photo: "https://media.giphy.com/media/l3mZcqogHCmOCvsVq/giphy.gif",
+        scores: ["1","5","5","5","4","5","5","5","4","5"]
+    },
+    {
+        name:"Nona",
+        photo: "https://media.giphy.com/media/l3mZcqogHCmOCvsVq/giphy.gif",
+        scores: ["2","2","2","5","5","5","2","4","4","5"]
     }
+
 ];
+
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,11 +9,60 @@ module.exports = function(app)
     
   app.post("/api/friends", function(req, res) {
 
+    function add(a,b){
+        return a + b;
+    }
+
+    var newFriend = req.body;
+    var newScoreString = Object.values(newFriend)[2];
+    var newScoreInteger=[];
+
+    var bestMatch = {
+        name: "",
+        photo: "",
+        friendDifference: 1000
+    };
+
+   
+
+    var scoresInteger=[];
+
+    console.log(newScoreString);
+    
+        for (var i=0; i<newScoreString.length; i++){
+            newScoreInteger.push(parseInt(newScoreString[i]));
+        }   
+   
+    // console.log(newScoreInteger);
+
+    var newScore = newScoreInteger.reduce(add, 0);
+    // console.log(newScore);
+
+
+    for (var i=0; i < friends.length; i++){
+        var currentScore = friends[i].scores;
+        for (var i=0; i<currentScore.length; i++){
+            scoresInteger.push(parseInt(currentScore[i]));
+        }  
+        
+        var number = scoresInteger.reduce(add,0);
+
+        if (number === newScore){
+            console.log("new match")
+            // bestMatch[0].name = friends[i].name;
+			// bestMatch[0].photo = friends[i].photo;
+        } 
+
+    }
+
+    console.log(scoresInteger);
+
+
     friends.push(req.body);
-    res.json(friends);
+    res.json(bestMatch);
 
   });
-         
+        
 }
 
 
